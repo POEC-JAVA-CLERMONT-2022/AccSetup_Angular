9efb0d4e90638f6db1b1c21fc191bc23423c3a09
vincent <baidem@gmail.com>
2022-05-17 15:51:53 +0200

    component session-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SessionViewComponent} from "./session-view/session-view.component";
+import {SessionListComponent} from "./session-list/session-list.component"
 
 
 const routes: Routes = [
   { path: 'session', component: SessionViewComponent },
+  { path: 'list', component: SessionListComponent },
   { path: '', component: SessionViewComponent }
 ];
 
